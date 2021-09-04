2c828deb2f2a9e68d6baf41b5d48704a7f2ce8ec
dyson1602 <cclark000@citymail.cuny.edu>
2021-09-04 15:36:48 -0400

    Update thru pg 62

--- main.tf
+++ main.tf
@@ -2,11 +2,51 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0c55b159cbfafe1f0"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, world" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
