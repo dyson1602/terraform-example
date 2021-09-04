ddd3208259641aca4b9130545d17bfc4665bd3da
dyson1602 <cclark000@citymail.cuny.edu>
2021-09-04 15:47:30 -0400

    Update thru pg 64

--- main.tf
+++ main.tf
@@ -12,7 +12,11 @@ resource "aws_launch_configuration" "example" {
               echo "Hello, world" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-
+  # Required when using a launch config with an auto scaling group
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
   tags = {
     Name = "terraform-example"
   }
@@ -20,6 +24,7 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
 
   min_size = 2
   max_size = 10
@@ -40,6 +45,13 @@ resource "aws_security_group" "instance" {
   }
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type = number
